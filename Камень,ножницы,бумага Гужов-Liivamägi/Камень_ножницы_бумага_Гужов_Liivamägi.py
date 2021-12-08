98c354ed181c82e9aedc3e645f6735d774514046
vlademir guzov <infoman724@gmail.com>
2021-12-08 10:09:29 +0200

    finish 2

--- Камень,ножницы,бумага Гужов-Liivamägi/Камень_ножницы_бумага_Гужов_Liivamägi.py
+++ Камень,ножницы,бумага Гужов-Liivamägi/Камень_ножницы_бумага_Гужов_Liivamägi.py
@@ -1,14 +1,20 @@
 from random import*
 KNB=["k", "n", "b"]
-text=str(input("Играем в камень ножницы бумага! игаем с AI?/Player?"))
+text=str(input("Играем в камень ножницы бумага! игаем с AI?/Player ?"))
+if text=="AI":
+    print("Вы можете выбрать:Камень-k,Ножницы и Бумагу-b")
+    print()
+    print("Ответы БОТА:0-k,1-n,2-b")
+    print()
 while text=="AI":
      while 1:
+        print()
         a=str(input("player1>>>"))
         m=KNB.index(a)
         print()
         BOT=randint(0,2)
-        print(BOT)
-   
+        print("BOT кидает:",BOT)
+        print()
         if m==BOT :
             print("ничья")
         elif m==2 and BOT==0 or m==0 and BOT==1 or m==1 and BOT==2 :
@@ -16,4 +22,7 @@ while text=="AI":
             print()
         else:
             print("BOT win")
+
+
+
             
